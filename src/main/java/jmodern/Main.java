88cfbd92e93738f6197c77a3eab5b357bd39e01b
Jacob Morzinski <jmorzins@mit.edu>
2015-12-11 23:59:54 -0500

    Logging

--- src/main/java/jmodern/Main.java
+++ src/main/java/jmodern/Main.java
@@ -4,22 +4,29 @@ import co.paralleluniverse.fibers.Fiber;
 import co.paralleluniverse.strands.Strand;
 import co.paralleluniverse.strands.channels.Channel;
 import co.paralleluniverse.strands.channels.Channels;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+    static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) throws Exception {
         final Channel<Integer> ch = Channels.newChannel(0);
 
         new Fiber<Void>(() -> {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 100000; i++) {
                 Strand.sleep(100);
+                log.info("Sending {}", i); // log something
                 ch.send(i);
+                if (i % 10 == 0)
+                    log.warn("Sent {} messages", i + 1); // log something
             }
             ch.close();
         }).start();
 
         new Fiber<Void>(() -> {
             Integer x;
-            while((x = ch.receive()) != null)
+            while ((x = ch.receive()) != null)
                 System.out.println("--> " + x);
         }).start().join(); // join waits for this fiber to finish
     }
